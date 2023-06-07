23ac4ee62c8a5195e871e88a78872bd9794034c9
user <sandipjayaswal@gmail.com>
2023-06-07 12:15:58 +0530

    add to cart design done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import User from './User';
+import HomeContainer from './containers/HomeContainer'
 
 function App() {
   return (
     <div className="App">
-       {/* <h1>App Component</h1> */}
-       <User data= {{name :"Sandip kumar",age:"26"}}/>
-    </div>
+      <HomeContainer/>
+          </div>
   );
 }
 
