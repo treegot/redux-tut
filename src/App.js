9ac4bb2aa2997d92e45867e0d85edc1389d7b3ef
user <sandipjayaswal@gmail.com>
2023-06-07 14:49:00 +0530

    third comment

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import User from './User';
 import HomeContainer from './containers/HomeContainer'
+import HeaderContainer from './containers/HeaderContainer'
 
 function App() {
   return (
     <div className="App">
+    <HeaderContainer/>
       <HomeContainer/>
           </div>
   );
